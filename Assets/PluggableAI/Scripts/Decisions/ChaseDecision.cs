775fc64cd2b5615650bb324be57ab0d467287cdd
Kanbaru <lihaochen910@hotmail.com>
2017-09-05 10:36:21 +0800

    添加 AI状态条件判定脚本

---
+++ Assets/PluggableAI/Scripts/Decisions/ChaseDecision.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 判定追踪玩家是否超出了最大范围/玩家死亡
+/// </summary>
+[CreateAssetMenu(menuName = "PluggableAI/Decisions/Chase")]
+public class ChaseDecision : Decision
+{
+    public float MaxChaseDistance = 10f;
+    public override bool Decide(StateController controller)
+    {
+        // 目标Transform丢失
+        if (!controller.chaseTarget)
+            return false;
+
+        // 目标是否超出范围
+        return getDistance(controller) < MaxChaseDistance ? true : false;
+    }
+
+    private float getDistance(StateController controller)
+    {
+        return Vector3.Distance(controller.transform.position, controller.chaseTarget.position);
+    }
+}
---
+++ Assets/PluggableAI/Scripts/Decisions/LookDecision.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "PluggableAI/Decisions/Look")]
+public class LookDecision : Decision
+{
+    public override bool Decide(StateController controller)
+    {
+        bool isVisiable = Look(controller);
+
+        return isVisiable; 
+    }
+
+    private bool Look(StateController controller)
+    {
+        RaycastHit hit;
+
+        Debug.DrawRay(controller.eyes.position,controller.eyes.forward.normalized * controller.enemyStats.lookRange,Color.green);
+
+        if (Physics.SphereCast(
+            controller.eyes.transform.position,
+            controller.enemyStats.lookSphereCastRadius,
+            controller.eyes.forward.normalized,
+            out hit, controller.enemyStats.lookRange) &&
+            hit.transform.CompareTag("Player"))
+        {
+            controller.chaseTarget = hit.transform;
+            return true;
+        }
+        else return false;
+    }
+}
