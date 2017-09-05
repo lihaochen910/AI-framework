fd8e0e54877f833204856d6f4188280d3d10a93e
Kanbaru <lihaochen910@hotmail.com>
2017-09-05 10:34:37 +0800

    添加 AI模块脚本

---
+++ Assets/PluggableAI/Scripts/AIFramework/Action.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+/// <summary>
+/// 行为抽象类
+/// </summary>
+public abstract class Action : ScriptableObject {
+    /// <summary>
+    /// 行为具体实现
+    /// </summary>
+    /// <param name="controller">状态控制器</param>
+    public abstract void Act(StateController controller);
+}
---
+++ Assets/PluggableAI/Scripts/AIFramework/Decision.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+/// <summary>
+/// 条件判定具体实现类
+/// </summary>
+public abstract class Decision : ScriptableObject
+{
+    /// <summary>
+    /// 判定方法
+    /// </summary>
+    /// <param name="controller"></param>
+    /// <returns>判定是否成立</returns>
+    public abstract bool Decide(StateController controller);
+}
