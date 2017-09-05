75ff9516450e617d7de2f2465b9d5ac522115b8c
Kanbaru <lihaochen910@hotmail.com>
2017-09-05 10:35:56 +0800

    添加 AI行为实现脚本

---
+++ Assets/PluggableAI/Scripts/Actions/AttackAction.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 向玩家发射子弹
+/// </summary>
+[CreateAssetMenu(menuName = "PluggableAI/Actions/Attack")]
+public class AttackAction : Action
+{
+    public override void Act(StateController controller)
+    {
+        Attack(controller);
+    }
+    private void Attack(StateController controller)
+    {
+        controller.tankShooting.Fire(controller.enemyStats.attackForce, controller.enemyStats.attackRate);
+    }
+}
---
+++ Assets/PluggableAI/Scripts/Actions/ChaseAction.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 追踪玩家的行为实现
+/// </summary>
+[CreateAssetMenu(menuName = "PluggableAI/Actions/Chase")]
+public class ChaseAction : Action
+{
+    public override void Act(StateController controller)
+    {
+        Chase(controller);
+    }
+
+    private void Chase(StateController controller)
+    {
+        Debug.Assert(controller.chaseTarget != null,"追踪目标为空");
+
+        controller.navMeshAgent.destination = controller.chaseTarget.position;
+        controller.navMeshAgent.Resume();
+    }
+}
